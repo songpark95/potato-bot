73bffab02a6bdba75c67c0c56c53f414e4d234ae
Baks <baksbaksbaksyt@gmail.com>
2020-08-23 02:12:52 -1000

    separated commands to ./commands

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
+const fs = require('fs');
 const Discord = require('discord.js');
-const client = new Discord.Client();
 const { prefix, token} = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+// dynamically retrieve command files from commands folder 
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+    // setting a new item in the collection with the key as command name and value as exported module!
+    client.commands.set(command.name, comnmand);
+}
+
 
 
 // client ready check. 
@@ -13,7 +26,7 @@ client.once('ready', () => {
 client.on('message', message => {
     if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-    const args = message.content.slice(prefix.length).trim().split(' ');
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
     const command = args.shift().toLowerCase();
     
     if (command === 'server-info') {
@@ -23,10 +36,12 @@ client.on('message', message => {
             return message.channel.send(`You didnt provide any arguments, ${message.author}.`);
         } 
         message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+    } else if(command === 'user-info') {
+        message.channel.send(`User name: ${message.author.username}\nUser ID: ${message.author.id}`);
     }
 
     }
-);
+)
 
 
 
