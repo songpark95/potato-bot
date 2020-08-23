e96bb574cc1ef950c352cd50a77efe24d067f163
Baks <baksbaksbaksyt@gmail.com>
2020-08-23 01:33:12 -1000

    modified to remove token data from main javascript file.

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { prefix, token} = require('./config.json');
+
+
 // client ready check. 
 client.once('ready', () => {
     console.log(`Logged in as ${client.user.tag}`);
 });
 
+// pulls user info and logs it to the system upon request.
+
 client.on('message', message => {
-    console.log(`${member} said ${message.content}`);
-});
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(' ');
+    const command = args.shift().toLowerCase();
+    
+    if (command === 'server-info') {
+        message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
+    } else if(command === 'info') {
+        if (!args.length) {
+            return message.channel.send(`You didnt provide any arguments, ${message.author}.`);
+        } 
+        message.channel.send(`Command name: ${command}\nArguments: ${args}`);
+    }
+
+    }
+);
 
 
 
@@ -20,4 +39,7 @@ client.on('message', message => {
 
 
 
-client.login('Mzc3MjcwNzIwNjM3Njk4MDQ5.WgEOvQ.a1jBC-k0qZQoW_Terbsuho3M-bE');
+
+
+
+client.login(token);
